9d9ecd787dbfaa8ef6d43fce5d0c1efc65f7edae
saket98 <saket.pawar98@gmail.com>
2021-05-06 21:41:39 +0530

    route to insert the data  added

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const studentRoute = require("./routes/student.route");
 
 require("dotenv").config();
+
+mongoose
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+		useCreateIndex: true,
+	})
+	.then(() => {
+		console.log("mongodb connected.");
+	})
+	.catch((err) => console.log(err.message));
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,9 +25,11 @@ app.use(
 	})
 );
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+app.get("/", (req, res) => {
+	res.send("Hello World");
+});
+
+app.use("/student", studentRoute);
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
